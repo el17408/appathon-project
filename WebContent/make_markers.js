307e31d16b7b50d5158f1733d57239c235c73483
jim <jim1997jim@hotmail.com>
2020-08-05 14:45:00 +0300

    Final version

--- WebContent/make_markers.js
+++ WebContent/make_markers.js
@@ -1,3 +1,97 @@
+
+$(document).ready(function() {
+    // execute
+	(function() {   
+			
+	    var input = getUrlVars()["search"];
+		if (input === "" || input === " " || input == undefined || input == null) {
+			return;
+		}
+		input = input.split("+");	//remove all occurances of +
+		input = input.join(' ');	// and replace them with a space
+		 if (getUrlVars()["format"] == "map"){
+			createOutput(JSON.parse(httpGet("http://localhost:8080/appathon_project/sql_request.php?input="+input)),"map");
+		}
+		else if(getUrlVars()["format"] == "json") {
+			createOutput(JSON.parse(httpGet("http://localhost:8080/appathon_project/sql_request.php?input="+input)),"json");
+	    		}
+	    return;
+	    })();
+	});
+	    	
+		function getUrlVars() {
+    	        var vars = {};
+    	        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+            vars[key] = value;
+        });
+        return vars;
+}
+	function createOutput( output , format ) {
+		var toReturn = [];
+		
+		for(var i = 0; i < output.length; i++) {
+			var request = JSON.parse(httpGet("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDR919QvVF2Y632fs9uwdASsOAiVaykJd4&address="+output[i].country_name));
+			if (request.status == "ZERO_RESULTS") {
+				continue;
+			}
+			else{
+			
+				output[i]["lat"] = request.results[0].geometry.location.lat;
+				
+				output[i]["lng"] = request.results[0].geometry.location.lng;
+				toReturn.push(output[i]);
+			}
+		}
+		if (format == "map"){
+			return makeMarkers(toReturn);
+		}
+		else {
+			if(toReturn.length == 0){
+				document.body.innerHTML= "{status : Zero_Results}";
+				return;
+			}
+			toReturn.push({status:200});
+			document.body.innerHTML= JSON.stringify(toReturn);
+			return;
+		}
+	}
+	
+	function httpGet(theUrl)
+	{
+		if (window.XMLHttpRequest){
+		 	var xmlHttp = new XMLHttpRequest();
+		 }
+		 else {
+			 var xmlHttp = new ActiveObject("Microsoft.XMLHTTP");
+		 }
+	    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+	    xmlHttp.send( null );
+	    return xmlHttp.responseText;
+	}
+	
+	
+	function makeMarkers(arr){
+		var options = {
+	                    zoom: 2,
+	                    center: new google.maps.LatLng(45.8665231, -6.9240942), 
+	                    mapTypeId: google.maps.MapTypeId.TERRAIN,
+	                    mapTypeControl: false
+	                };
+	
+	    // init map
+		var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+		for (var i = 0; i < arr.length; i++) {
+			// init markers
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(arr[i].lat, arr[i].lng),
+				map : map,
+				title: arr[i].country_name,
+				label : arr[i].cnt
+			});
+		}
+	}
+
+
 //synchronous
 function httpGet2(theUrl)
 {
@@ -13,7 +107,7 @@ function httpGet2(theUrl)
 }
 
 //asynchronous
-function httpGet(theUrl)
+function httpGet3(theUrl)
 {
 	if (window.XMLHttpRequest){
 	 	var xmlHttp = new XMLHttpRequest();
