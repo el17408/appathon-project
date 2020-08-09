5d7f713ccd236c84945beda528f280e78c044e55
jim <jim1997jim@hotmail.com>
2020-08-09 18:39:13 +0300

    Removed doGet

--- src/appathon_ntua/Servlet.java
+++ src/appathon_ntua/Servlet.java
@@ -9,10 +9,6 @@ import java.sql.*;
 
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		try
